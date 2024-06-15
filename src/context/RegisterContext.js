980ef8fe6351b61728cf1c678ed10cdaca654033
mahesh <pmahesh493605@gmail.com>
2024-06-15 07:24:32 +0000

    Meetup

---
+++ src/context/RegisterContext.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const RegisterContext = React.createContext({
+  name: '',
+  topic: '',
+  changeName: () => {},
+  changeTopic: () => {},
+  isRegistered: false,
+  registerName: () => {},
+  showError: false,
+  updateError: () => {},
+})
+export default RegisterContext
